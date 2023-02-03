3661c60d02c1347de462c564468c01e9d27a9376
athiragithubworld <athirahrms@gmail.com>
2023-02-03 21:09:53 +0530

    src folder

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -2,12 +2,7 @@ function ExpenseItem(){
     return (
         <div>
             <h2>Expense Item!!!</h2>
-            {/* <ul title="Expense Item" className="Expense Item">
-                <li>Food Rs 10</li>
-                <li>Petrol Rs 100</li>
-                <li>Movies Rs 200</li>
-                
-            </ul> */}
+            
             <p>Expense Items</p>
             <p>Food Rs 10</p>
             <p>Petrol Rs 100</p>
