a84a1c882bb8c457df0258ceca9d65f95dcf0aa1
athiragithubworld <athirahrms@gmail.com>
2023-02-07 21:34:17 +0530

    Expenseitem.js file (Adding map function to get the list)

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,28 +1,26 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(){
-    const expenseDate = new Date(2023 , 23,27);
-    const expenseTitle = ' Car Insurance';
-    const expenseAmount = 298.67;
-    const LocationOfExpenditure = 'Mumbai'
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2023, 23, 27);
+  // const expenseTitle = " Car Insurance";
+  // const expenseAmount = 298.67;
+  // const LocationOfExpenditure = "Mumbai";
 
-    return (
-        <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
-            <div className="expense-item__description">
-              <h2>{expenseTitle}</h2>
-              <div className="Location_Of_Expenditure">{LocationOfExpenditure}</div>
-              <div className="expense-item__price"> ${expenseAmount}</div>
-              
-            </div>
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="Location_Of_Expenditure">{props.LocationOfExpenditure}</div>
+        <div className="expense-item__price"> ${props.amount}</div>
+      </div>
 
-            {/* <h2>Expense Item!!</h2>
+      {/* <h2>Expense Item!!</h2>
             <p>Expense Items</p>
             <p>Food Rs 10</p>
             <p>Petrol Rs 100</p>
             <p>Movies Rs 200</p> */}
-        </div>
-     
-    );
+    </div>
+  );
 }
 export default ExpenseItem;
