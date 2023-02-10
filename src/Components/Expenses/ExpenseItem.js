1934689a4e1e92c7510821995f64c5961df4efa9
athiragithubworld <athirahrms@gmail.com>
2023-02-10 08:31:08 +0530

    Expenses.js (Organise the file to Expenses and Add Arrow Function)

---
+++ src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  // const expenseDate = new Date(2023, 23, 27);
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>{/* can write this way <ExpenseDate/> */}
+      <ExpenseDetails
+          title={props.title}
+          LocationOfExpenditure={props.LocationOfExpenditure}
+          amount={props.amount}
+        ></ExpenseDetails>
+     
+      {/* <h2>Expense Item!!</h2>
+            <p>Expense Items</p>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movies Rs 200</p> */}
+    </div>
+  );
+}
+export default ExpenseItem;
