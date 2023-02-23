638aa214cd5a114b47f5e40c07cf4d4b3c3e514a
athiragithubworld <athirahrms@gmail.com>
2023-02-23 17:04:28 +0530

    ExpenseItem.js (Some updation)

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,46 +1,60 @@
-import React, { useState } from "react";
+import React from "react";
 
 
 import ExpenseDetails from "./ExpenseDetails";
 
+// import ExpensesFilter from "./ExpensesFilter";
+
 import "./ExpenseItem.css";
-// import Card from "../UI/Card";
+import Card from "../UI/Card";
+
+
+
+
 
 const ExpenseItem = (props) => {
-  // const expenseDate = new Date(2023, 23, 27);
-  const [title, setTitle] = useState(props.title);
+
+  // const [filteredYear ,setFilteredYear] =useState('2020');
+
+  // const filterChangeHandler = selectedYear =>{
+  //   setFilteredYear(selectedYear);
+  // };
+
+
+
+
+  // // const expenseDate = new Date(2023, 23, 27);
+  // const [title, setTitle] = useState(props.title);
   
-  const clickHandler = () => {
-    setTitle("Updated");
+  // const clickHandler = () => {
+  //   setTitle("Updated");
     
     
-  };
-  console.log(title);
-  const [amount, setAmount] = useState(props.amount);
-  const clickHandler2 = () => {
-    setAmount(100);
-    console.log(amount);
-  };
+  // };
+  
+  // const [amount, setAmount] = useState(props.amount);
+  // const clickHandler2 = () => {
+  //   setAmount(100);
+  //   console.log(amount);
+  // };
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       
       {/* <ExpenseDate date={props.date}></ExpenseDate>  */}
       {/* can write this way <ExpenseDate/> */}
+
       <ExpenseDetails
         date={props.date}
-        title={title}
+        title={props.title}
         LocationOfExpenditure={props.LocationOfExpenditure}
-        amount={amount}
+        amount={props.amount}
       ></ExpenseDetails>
-      <button onClick={clickHandler}>change Title</button>
-      <button onClick={clickHandler2}>change Expense</button>
-      {/* <h2>Expense Item!!</h2>
-            <p>Expense Items</p>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p> */}
-    </div>
+
+      {/* <button onClick={clickHandler}>change Title</button>
+      <button onClick={clickHandler2}>change Expense</button> */}
+      
+    </Card>
   );
 };
 export default ExpenseItem;
