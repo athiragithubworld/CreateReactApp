8d24adeaf798ef22a70f02e032c41d8acfa787b9
athiragithubworld <athirahrms@gmail.com>
2023-02-25 20:07:40 +0530

    NewExpense.js file (AddNewExpense button  and cancel button are added to open and close the Expense form)

--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,3 +1,5 @@
+import React,{useState} from "react";
+
 import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
@@ -10,12 +12,29 @@ const NewExpense = (props) => {
       };
       // console.log(expenseData);
       props.onAddExpense(expenseData);
+      setIsEditing (false);
     };
 
+    
+  const [IsEditing , setIsEditing] = useState(false);
+  
+  const startEditingHandler = () => {
+    setIsEditing (true);
+    console.log("Add new expense");
+  }
+
+  const stopEditingHandler = () =>{
+    setIsEditing (false);
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData ={saveExpenseDataHandler}></ExpenseForm>
+      {!IsEditing && <button onClick={startEditingHandler}>Add New Expense</button>} 
+      {IsEditing && 
+      <ExpenseForm 
+      onSaveExpenseData ={saveExpenseDataHandler}
+      onCancel = {stopEditingHandler}
+      ></ExpenseForm>}
     </div>
   );
 };
