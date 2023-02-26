35a2c1bee96346d08f6a8081847a94d2ae24eed7
athiragithubworld <athirahrms@gmail.com>
2023-02-26 16:27:42 +0530

    ExpenseItem.js file (some update)

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 import ExpenseDetails from "./ExpenseDetails";
 
 // import ExpensesFilter from "./ExpensesFilter";
@@ -8,30 +7,21 @@ import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
-
-
-
-
 const ExpenseItem = (props) => {
-
   // const [filteredYear ,setFilteredYear] =useState('2020');
 
   // const filterChangeHandler = selectedYear =>{
   //   setFilteredYear(selectedYear);
   // };
 
-
-
-
   // // const expenseDate = new Date(2023, 23, 27);
   // const [title, setTitle] = useState(props.title);
-  
+
   // const clickHandler = () => {
   //   setTitle("Updated");
-    
-    
+
   // };
-  
+
   // const [amount, setAmount] = useState(props.amount);
   // const clickHandler2 = () => {
   //   setAmount(100);
@@ -40,7 +30,6 @@ const ExpenseItem = (props) => {
 
   return (
     <Card className="expense-item">
-      
       {/* <ExpenseDate date={props.date}></ExpenseDate>  */}
       {/* can write this way <ExpenseDate/> */}
 
@@ -53,7 +42,6 @@ const ExpenseItem = (props) => {
 
       {/* <button onClick={clickHandler}>change Title</button>
       <button onClick={clickHandler2}>change Expense</button> */}
-      
     </Card>
   );
 };
